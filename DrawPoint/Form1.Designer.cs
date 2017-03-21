3897297297e60702bb268d7bcfac5bd5a64849c8
rexbean <rexbean36@gmail.com>
2017-03-21 09:26:12 -0700

    draw dynamic input point

---
+++ DrawPoint/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace DrawPoint
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ShowButton = new System.Windows.Forms.Button();
+            this.ReadInButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.Location = new System.Drawing.Point(630, 97);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(75, 23);
+            this.ShowButton.TabIndex = 1;
+            this.ShowButton.Text = "显示";
+            this.ShowButton.UseVisualStyleBackColor = true;
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
+            // 
+            // ReadInButton
+            // 
+            this.ReadInButton.Location = new System.Drawing.Point(630, 31);
+            this.ReadInButton.Name = "ReadInButton";
+            this.ReadInButton.Size = new System.Drawing.Size(75, 23);
+            this.ReadInButton.TabIndex = 2;
+            this.ReadInButton.Text = "读入";
+            this.ReadInButton.UseVisualStyleBackColor = true;
+            this.ReadInButton.Click += new System.EventHandler(this.ReadInButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoScrollMinSize = new System.Drawing.Size(6000, 0);
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(3, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(622, 337);
+            this.panel1.TabIndex = 3;
+            this.panel1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.panel1_Scroll);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(-175, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65535, 314);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(630, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(631, 170);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(631, 198);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 350);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ReadInButton);
+            this.Controls.Add(this.ShowButton);
+            this.Name = "MainForm";
+            this.Text = "主界面";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ShowButton;
+        private System.Windows.Forms.Button ReadInButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ DrawPoint/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace DrawPoint
+{
+    public partial class MainForm : Form
+    {
+        List<Point> signal = new List<Point>();
+       
+        public MainForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ReadInButton_Click(object sender, EventArgs e)
+        {
+
+            string path = "c:\\speech.dat";
+            FileStream sr = new FileStream(path, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(sr);
+
+
+
+            int a = 0;
+            int low = 0;
+            int high;
+            for (int i = 0; i < 95658; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    low = sr.ReadByte();
+                }
+                else
+                {
+                    high = sr.ReadByte();
+                    a = (int)(high * Math.Pow(2, 8) + low);
+                    if (a > 32768)
+                    {
+                        a -= 65536;
+                    }
+                    Point p = new Point(i, a);
+                    signal.Add(p);
+                }
+            }
+
+           
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void show(Graphics g,Pen pen,int j)
+        {
+           
+ 
+        }
+
+        private void panel1_Scroll(object sender, ScrollEventArgs e)
+        {
+            textBox1.Text = this.panel1.HorizontalScroll.Value.ToString();
+            textBox2.Text = signal[this.panel1.HorizontalScroll.Value].X.ToString();
+            textBox3.Text = signal[this.panel1.HorizontalScroll.Value].Y.ToString();
+
+            //show(this.panel1.HorizontalScroll.Value);
+            Bitmap bm = new Bitmap(7000, 337);
+            Graphics g = Graphics.FromImage(bm);
+            Pen pen = new Pen(Color.White, 1);
+            for (int i = this.panel1.HorizontalScroll.Value; i < this.panel1.HorizontalScroll.Value + 600; i++)
+            {
+                //g.Clear(pictureBox1.BackColor);
+                g.DrawLine(pen, signal[i].X
+                    - this.panel1.HorizontalScroll.Value
+                    , signal[i].Y + 100,
+                   signal[i].X + 1
+                   - this.panel1.HorizontalScroll.Value
+                   , signal[i].Y + 100);
+            }
+            pictureBox1.Image = bm;
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+            
+
+
+
+        }
+    }
+}
